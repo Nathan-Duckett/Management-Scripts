d39803f82f68c4864ab645266bdccab08e30cbb9
Nathan Duckett <nduckett13@gmail.com>
2020-01-05 18:28:10 +1300

    Fixed unnecessary sudo privilege. Made sure update runs on remote machine

--- maintain/update.bash
+++ maintain/update.bash
@@ -1,15 +1,11 @@
-#!/bin/bash
-
-if [ $EUID != 0 ]; then
-	echo "This script must be run as root user"
-	exit
-fi
+#/bin/bash
 
 ssh_host=Home_Server
 
+# Run updates
 echo "Updating applications"
-ssh -t $ssh_host 'apt update' > /dev/null
-ssh -t $ssh_host 'apt upgrade -y' > /dev/null
+ssh -t $ssh_host 'sudo apt update -qq && sudo apt upgrade -qq' > /dev/null
 
+# Restart docker services to work correctly with updates
 echo "Restarting Services"
-bash ./restart-all-docker.bash > /dev/null
+ssh -t $ssh_host 'docker restart $(docker ps -q)' > /dev/null
